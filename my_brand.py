a7930c2dc85cccaa959eb232634db28466aa2d34
npal312 <102007313+npal312@users.noreply.github.com>
2023-02-23 01:13:46 -0500

    Create my_brand.py

---
+++ my_brand.py
@@ -0,0 +1,9 @@
+import datetime
+
+def my_brand(assignment):
+    time = str(datetime.datetime.now())
+    print("=*=* Nicholas Palladino *=*=")
+    print("=*=* Course 2023: SSW-567-A *=*=")
+    print("=*=* " + assignment + " *=*=")
+    print("=*=* " + time + " *=*=")
+    print()
